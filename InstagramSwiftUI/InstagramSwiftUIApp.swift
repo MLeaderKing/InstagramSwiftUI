03585c32c30e36f7843718f68b13ec658f57207f
Nasir Farmani <nasir.farmani@icloud.com>
2022-04-07 12:06:28 +0200

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Nasir Farmani on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
